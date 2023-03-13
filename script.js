1c741287293e6d5b9edbcd8a53c1f0c1b4a98f61
Lucija Filipovic <filipovic2018@gmail.com>
2023-03-13 17:50:56 +0100

    Caesar-Chiffre Einstieg
    1. Was ist Caesar-Chiffre?
    &
    2. Wie wandelt man eine Buchstabe einfach um?

---
+++ script.js
@@ -0,0 +1,26 @@
+// Aufgabe 1
+
+//  Zusammenfassung zu Buschstaben Verschieben 
+
+
+// 1. Was ist Caesar-Chiffre? 
+
+// Verschiebung der Buchstaben um eine Verschlüsselung zu kriegen, z.B. wenn man das 
+// leteinische Alphabet um 3 Stellen verschiebt sieht es so aus:
+
+//         {a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z} wird zu:
+//         {d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, a, b, c}
+
+
+
+
+//  2. Wie wandelt man eine Buchstabe einfach um?
+
+//              prompt("say a")
+
+//              function converter(){
+//                  if(prompt="a");
+//                  console.log("1")
+//               }
+
+//              converter();
